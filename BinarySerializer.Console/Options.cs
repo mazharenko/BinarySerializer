3b0ea20330d0f6055a1675c09cbb1efcb0c94284
Alexey Mazharenko <mazharenko.a@yandex.ru>
2017-05-05 07:10:16 +0500

    console - serialization

---
+++ BinarySerializer.Console/Options.cs
@@ -0,0 +1,41 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace BinarySerializer.Console
+{
+    public class Options
+    {
+        [HelpVerbOption]
+        public string GetUsage(string verb)
+        {
+            return HelpText.AutoBuild(this, verb);
+        }
+
+        [VerbOption("serialize", HelpText = "Perform a serialization operation")]
+        public SerializeOptions SerializeVerb { get; set; }
+
+        [VerbOption("deserialize", HelpText = "Perform a deserialization operation")]
+        public DeserializeOptions DeserializeVerb { get; set; }
+    }
+
+    public class SerializeOptions
+    {
+        [Option('t', "type", Required = true, HelpText = "The type of an object.")]
+        public string InputType { get; set; }
+
+        [Option('a', "assembly", HelpText = "The assembly to lookup the specified type")]
+        public string Assembly { get; set; }
+
+        [Option('i', "input", HelpText = "File containing an object to serialize")]
+        public string Input { get; set; }
+
+        [Option('o', "output", HelpText = "File-destination for the serialized object. If exists, will be overwritten")]
+        public string Output { get; set; }
+    }
+
+    public class DeserializeOptions
+    {
+
+    }
+
+}
---
+++ BinarySerializer.Console/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Reflection;
+using Newtonsoft.Json;
+
+namespace BinarySerializer.Console
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+
+                string invokedVerb = null;
+                object invokedVerbInstance = null;
+
+                var options = new Options();
+                if (!CommandLine.Parser.Default.ParseArguments(args, options,
+                    (verb, subOptions) =>
+                    {
+                        // if parsing succeeds the verb name and correct instance
+                        // will be passed to onVerbCommand delegate (string,object)
+                        invokedVerb = verb;
+                        invokedVerbInstance = subOptions;
+                    }))
+                {
+                    Environment.Exit(CommandLine.Parser.DefaultExitCodeFail);
+                }
+
+                if (invokedVerb == "serialize")
+                {
+                    Serialize((SerializeOptions)invokedVerbInstance);
+                }
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(e);
+                Environment.Exit(1);
+            }
+        }
+
+        public static void Serialize(SerializeOptions options)
+        {
+            string objectString;
+            Type type;
+
+            if (options.Assembly != null)
+            {
+                var assembly = Assembly.LoadFrom(Path.GetFullPath(options.Assembly));
+                type = assembly.GetType(options.InputType, true);
+            }
+            else
+                type = Type.GetType(options.InputType, true);
+
+            if (options.Input != null)
+            {
+                objectString = File.ReadAllText(options.Input);
+            }
+            else
+            {
+                using (var reader = new StreamReader(System.Console.OpenStandardInput(8192)))
+                    objectString = reader.ReadToEnd();
+            }
+
+            var @object = JsonConvert.DeserializeObject(objectString, type);
+
+            Stream stream;
+            if (options.Output != null)
+                stream = File.Create(options.Output);
+            else
+                stream = System.Console.OpenStandardOutput();
+
+            using (stream)
+                ContractSerializer.Serialize(@object, stream);
+
+        }
+    }
+}
