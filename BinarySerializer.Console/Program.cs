0d8bfbccf9095a8db03d88c01d1b714471192d73
mazahrenko <mazharenko.a@yandex.ru>
2017-05-08 05:09:10 +0500

    errors are redirected to the error output

--- BinarySerializer.Console/Program.cs
+++ BinarySerializer.Console/Program.cs
@@ -33,7 +33,7 @@ namespace BinarySerializer.Console
             }
             catch (Exception e)
             {
-                System.Console.WriteLine(e);
+                System.Console.Error.WriteLine(e);
                 Environment.Exit(1);
             }
         }
